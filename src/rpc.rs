f285d4b53edf00e8e44e304d0715e08b0471b299
Sinmysize <jadenshellie01@gmail.com>
2026-05-16 22:41:20 -0400

    Separated RPC functions to its own file

---
+++ src/rpc.rs
@@ -0,0 +1,245 @@
+use std::{env::home_dir, fs, process::Command, thread, time::Duration};
+
+use discord_rich_presence::{DiscordIpc, DiscordIpcClient, activity::{self, Assets, Timestamps}};
+use rand::seq::IndexedRandom;
+
+use crate::{config::{CONFIG_PATH, Config}};
+
+#[derive(Debug, Default)]
+pub struct RPCState {
+    timestamp: i64,
+    icon: String,
+    icon_text: String,
+    small_icon: String,
+    small_text: String,
+    message: String,
+}
+
+impl RPCState {
+    pub fn new(config: &Config) -> Self {
+        let default_icon = match config.data.get("default_icon") {
+            Some(d) => {
+                if d.len() == 0 {
+                    "Empty"
+                } else {
+                    d[0].as_str()
+                }
+            },
+            None => "Empty"
+        };
+
+        let default_icon_text = match config.data.get("default_icon_text") {
+            Some(d) => {
+                if d.len() == 0 {
+                    "Made by Sinmysize"
+                } else {
+                    d[0].as_str()
+                }
+            },
+            None => "Made by Sinmysize"
+        };
+
+        let default_small_icon = match config.data.get("default_small_icon") {
+            Some(d) => {
+                if d.len() == 0 {
+                    "Empty"
+                } else {
+                    d[0].as_str()
+                }
+            },
+            None => "Empty"
+        };
+
+        let default_small_text = match config.data.get("default_small_text") {
+            Some(d) => {
+                if d.len() == 0 {
+                    "Using Linux"
+                } else {
+                    d[0].as_str()
+                }
+            },
+            None => "Using Linux"
+        };
+
+        Self {
+            timestamp: std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as i64,
+            icon: default_icon.to_string(),
+            icon_text: default_icon_text.to_string(),
+            small_icon: default_small_icon.to_string(),
+            small_text: default_small_text.to_string(),
+            message: "A Simple RPC Client.".to_string()
+        }
+    }
+
+    pub fn run_rpc(&mut self, config: &mut Config) -> Result<(), ()> {
+        config.read_config();
+
+        let mut temp_config = Config::new();
+        temp_config.read_config();
+
+        let mut cached_active = match temp_config.data.get("active") { 
+            Some(d) => d[0].to_string(),
+            None => "PLACEHOLDER".to_string() // This shouldn't be used at all, but you never know
+        };
+
+        let elapsed_time = Timestamps::new().start(self.timestamp);
+        let mut client = DiscordIpcClient::new(&config.data.get("clientId").expect("Failed to get [clientId] from config. Please check if [clientId] exists and has a valid ID")[0]);
+
+        match client.connect() {
+            Ok(_) => println!("Connected!"),
+            Err(_) => {
+                thread::sleep(Duration::from_millis(1_000));
+                println!("Trying to connect to RPC...");
+                self.run_rpc(config).unwrap();  
+            }
+        }
+
+        set_activity(
+            &mut client,
+            Some(&self.message),
+            Some(&self.icon),
+            Some(&self.icon_text),
+            None,
+            Some(&self.small_text),
+            Some(&self.small_icon),
+            elapsed_time.clone()
+        )
+        .expect("Something went wrong.");
+
+        loop {
+            thread::sleep(Duration::from_millis(10_000));
+            config.read_config();
+            
+            let mut temp_config = Config::new();
+            temp_config.read_config();
+
+            let current_active = temp_config.data.get("active").unwrap()[0].clone();
+ 
+            if current_active != cached_active {
+                println!("Swapped Config");
+
+                cached_active = current_active.clone();
+
+                config.file = fs::File::options()
+                .write(true)
+                .read(true)
+                .create(true)
+                .open(format!("{}/{}/{}", home_dir().unwrap().display(), CONFIG_PATH, current_active))
+                .unwrap();
+
+                config.read_config();
+
+                // Establish new client
+                client.client_id = config.data.get("clientId").unwrap()[0].clone();
+                client.reconnect().unwrap();
+            }
+
+            let messages = match config.data.get("messages") {
+                Some(d) => {
+                    if d.len() < 1 {
+                        &vec!["Check your config! [messages] is empty!".to_string()]
+                    } else {
+                        d
+                    }
+                },
+                None => &vec!["Check your config! [messages] is empty!".to_string()]
+            };
+
+            let default_icon = match config.data.get("default_icon") {
+                Some(d) => {
+                    if d.len() == 0 {
+                        "Empty"
+                    } else {
+                        d[0].as_str()
+                    }
+                },
+                None => "Empty"
+            };
+
+            let icons = match config.data.get("icons") {
+                Some(d) => {
+                    if d.len() < 1 {
+                        &vec!["Empty".to_string()]
+                    } else {
+                        d
+                    }
+                },
+                None => &vec![format!("{}", default_icon)]
+            };
+
+            let data = get_playerctl(config.data.get("player").cloned());
+            let music_format = format!("♪ {} - {}", data[0], if data.len() == 1 { "ᓚᘏᗢ ᶻ z Z" } else { &data[2] });
+
+            self.message = messages.choose(&mut rand::rng()).map(|v| &**v).unwrap().to_string();
+            self.icon = icons.choose(&mut rand::rng()).map(|v| &**v).unwrap().to_string();
+            let music: Option<&str> = Some(&music_format);
+            
+            match set_activity(
+                &mut client,
+            Some(&self.message),
+            Some(&self.icon),
+            Some(&self.icon_text),
+            music,
+            Some(&self.small_text),
+            Some(&self.small_icon),
+            elapsed_time.clone()
+            )
+            {
+                Ok(_) => {},
+                Err(_) => {
+                    println!("Something went wrong. Trying to reconnect...");
+                    self.run_rpc(config).unwrap();
+                }
+            }
+        }
+    }
+}
+
+fn get_playerctl(player: Option<Vec<String>>) -> Vec<String> {
+    let player = match &player {
+        Some(e) => {
+            if e.len() == 0 {
+                "Empty"
+            } else {
+                &e[0]
+            }
+        },
+        None => "Empty" 
+    };
+
+    let metadata = Command::new("playerctl")
+    .args(["-p", player, "metadata", "-f", "{{artist}}*{{album}}*{{title}}*{{length}}"])
+    .output()
+    .expect("Something went wrong in getting metadata.");
+
+    let output = String::from_utf8_lossy(&metadata.stdout).into_owned();
+    return output.split("*").map(|s| s.to_string()).collect::<Vec<String>>();
+}
+
+fn set_activity(
+    client: &mut DiscordIpcClient, 
+    text: Option<&str>, 
+    icon: Option<&str>, 
+    icon_text: Option<&str>,
+    music: Option<&str>,
+    small_text: Option<&str>,
+    small_icon: Option<&str>,
+    elapsed_time: Timestamps
+) -> Result<(), discord_rich_presence::error::Error> {
+    client.set_activity(
+        activity::Activity::new()
+        .details(text.unwrap_or("A Simple RPC Client."))
+        .state(music.unwrap_or("Loading LinuxRPC..."))
+        .assets(
+            Assets::new()
+            // Will default to discord placeholder icon! (Can change if manually building)
+            .large_image(icon.unwrap_or("Empty")) 
+            .large_text(icon_text.unwrap_or("Empty"))
+            .small_image(small_icon.unwrap_or("Empty"))
+            .small_text(small_text.unwrap_or("Empty"))
+        )
+        .timestamps(
+            elapsed_time.clone()
+        )
+    )
+}  
